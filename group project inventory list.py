fab8ecca5abadfc81849c8fd5863e39c6b5ebefa
gigimay06 <a.mayaka17@gmail.com>
2026-01-05 23:21:48 +0000

    Fix syntax errors in inventory list initialization

--- group project inventory list.py
+++ group project inventory list.py
@@ -1,25 +1,26 @@
 #make lists/ variables
 inventory =[
-    {"name":"cod","price":7.70,"category":"mains","quantity":0}
-    {"name":"salmon","price":9.50,"category":"mains","quantity":0}
-    {"name":"haddock","price":5.50,"category":"mains","quantity":0}
-    {"name":"mushroom","price":4.90,"category":"mains","quantity":0}
-    {"name":"plaice","price":8.0,"category":"mains","quantity":0}
-    {"name":"veggie","price":5.00,"category":"mains","quantity":0}
-    {"name":"fries","price":2.00,"category":"sides","quantity":0}
-    {"name":"onion rings","price":2.00,"category":"sides","quantity":0}
-    {"name":"calamari rings","price":3.00,"category":"sides","quantity":0}
-    {"name":"chiken nuggets","price":2.50,"category":"sides","quantity":0}
-    {"name":"fish fingers","price":2.50,"category":"sides","quantity":0}
-    {"name":"sausage small","price":2.00,"category":"sides","quantity":0}
-    {"name":"sausage large","price":3.00,"category":"sides","quantity":0} 
-    {"name":"mozzarella sticks","price":2.00,"category":"drinks","quantity":0}
-    {"name":"coke","price":1.0,"category":"drinks","quantity":0}
-    {"name":"sprite","price":0.80,"category":"drinks","quantity":0}
-    {"name":"tango","price":0.80,"category":"drinks","quantity":0}
-    {"name":"coke zero","price":0.80,"category":"drinks","quantity":0}
+    {"name":"cod","price":7.70,"category":"mains","quantity":0},
+    {"name":"salmon","price":9.50,"category":"mains","quantity":0},
+    {"name":"haddock","price":5.50,"category":"mains","quantity":0},
+    {"name":"mushroom","price":4.90,"category":"mains","quantity":0},
+    {"name":"plaice","price":8.0,"category":"mains","quantity":0},
+    {"name":"veggie","price":5.00,"category":"mains","quantity":0},
+    {"name":"fries","price":2.00,"category":"sides","quantity":0},
+    {"name":"onion rings","price":2.00,"category":"sides","quantity":0},
+    {"name":"calamari rings","price":3.00,"category":"sides","quantity":0},
+    {"name":"chiken nuggets","price":2.50,"category":"sides","quantity":0},
+    {"name":"fish fingers","price":2.50,"category":"sides","quantity":0},
+    {"name":"sausage small","price":2.00,"category":"sides","quantity":0},
+    {"name":"sausage large","price":3.00,"category":"sides","quantity":0},
+    {"name":"mozzarella sticks","price":2.00,"category":"drinks","quantity":0},
+    {"name":"coke","price":1.0,"category":"drinks","quantity":0},
+    {"name":"sprite","price":0.80,"category":"drinks","quantity":0},
+    {"name":"tango","price":0.80,"category":"drinks","quantity":0},
+    {"name":"coke zero","price":0.80,"category":"drinks","quantity":0},
     {"name":"juice","price":0.80,"category":"drinks","quantity":0}
 ]
+
 
 
 
